ca57e34b455700daafdfab7612ccdc3134e9d0c1
Altagracia Cierra <alcierra@va-h3.kzn.21-school.ru>
2022-01-17 20:01:53 +0300

    Refactor

--- ft_pipex.h
+++ ft_pipex.h
@@ -6,7 +6,7 @@
 /*   By: alcierra <alcierra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 19:26:57 by alcierra          #+#    #+#             */
-/*   Updated: 2022/01/17 19:28:10 by alcierra         ###   ########.fr       */
+/*   Updated: 2022/01/17 20:01:00 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 # define FT_PIPEX_H
 
 # include <stdlib.h>
+# include "libft/libft.h"
 
 #endif
--- main.c
+++ main.c
@@ -6,12 +6,11 @@
 /*   By: alcierra <alcierra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 19:33:45 by alcierra          #+#    #+#             */
-/*   Updated: 2022/01/17 19:53:55 by alcierra         ###   ########.fr       */
+/*   Updated: 2022/01/17 20:00:38 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_pipex.h"
-#include "libft/libft.h"
 
 #define ERR_PARAM_COUNT_MSG "Usage example:\
 \n./pipex infile \"ls -l\" \"wc -l\" outfile\n"
