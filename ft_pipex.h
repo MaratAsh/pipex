8e30fdd269d5cdb2372799700782f1ac7af80346
alcierra <alcierra@student.21-school.ru>
2022-03-22 14:53:42 +0300

    ft_pipex.h norminette

--- ft_pipex.h
+++ ft_pipex.h
@@ -6,7 +6,7 @@
 /*   By: alcierra <alcierra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 19:26:57 by alcierra          #+#    #+#             */
-/*   Updated: 2022/03/20 23:05:31 by alcierra         ###   ########.fr       */
+/*   Updated: 2022/03/22 14:53:09 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,17 +47,17 @@ t_command_fd	*ft_create_commfd(char *cmd);
 // ft_process_errors.c - for errors processing
 int				ft_validate_fd(int fd, char *file_name);
 
-
-char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-size_t	ft_strlen(char *s);
-int		ft_strncmp(const char *str1, const char *str2, size_t n);
-void	ft_putstr_fd(char *s, int fd);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-void	*ft_memset(void *b, int c, size_t len);
-char	**ft_split(char const *s, char c);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strjoin(char const *s1, char const *s2);
+size_t			ft_strlcpy(char *dst, const char *src, size_t size);
+size_t			ft_strlen(char *s);
+int				ft_strncmp(const char *str1, const char *str2, size_t n);
+void			ft_putstr_fd(char *s, int fd);
+void			*ft_memcpy(void *dst, const void *src, size_t n);
+void			*ft_calloc(size_t count, size_t size);
+void			*ft_memset(void *b, int c, size_t len);
+char			**ft_split(char const *s, char c);
+char			*ft_strnstr(const char *haystack,
+					const char *needle, size_t len);
 
 #endif
